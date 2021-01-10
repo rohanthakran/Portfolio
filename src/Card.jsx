504b4d812f9830c52918b845350b4df3f2133896
rohan <dec@gmail.com>
2021-01-10 11:54:26 +0530

    mobile responsive

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,7 +4,7 @@ const Card =(props) =>{
     return (
         <React.Fragment>
   
-                    <div className="col-md-3 col-10 m-5">
+                    <div className="col-sm-9 col-xs-4 col-md-4 col-lg-3 col-10 m-5">
                     <div className="card" >
 
                      <img src={props.shot} className="img-fluid" alt="Responsive image"/>
