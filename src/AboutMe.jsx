9967445c1a5d94b7770ad248d5f2a7d425d4b4a3
rohan <dec@gmail.com>
2021-01-11 16:38:50 +0530

    all done

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -1,6 +1,7 @@
 import React from  'react'
 import img1 from "./images/crop.jpg"
 import {Link} from  'react-router-dom'
+import {AiFillLinkedin,AiOutlineGithub} from 'react-icons/ai'
 const AboutMe =() =>{
         return(
             <React.Fragment>
@@ -17,6 +18,11 @@ const AboutMe =() =>{
                         <p>Hi! I am Rohan Thakran, a web developer/Mern Stack Developer focused on crafting great web experiences. I enjoy creating beautifully designed, intuitive and functional websites.</p>
                     </div>
                         </div>
+
+                     <div className="col-lg-2 text-center mx-auto icon2">
+            <AiFillLinkedin style={{color:"black", fontSize:"40px", marginRight:"20px", cursor: "pointer",  border:"2px solid black", borderRadius:"50%",padding:"5px"}}/>
+            <AiOutlineGithub style={{color:"black", fontSize:"40px", marginRight:"20px", cursor: "pointer", border:"2px solid black", borderRadius:"50%",padding:"5px"}}/>
+                     </div>   
                     </div>          
                             
                 
